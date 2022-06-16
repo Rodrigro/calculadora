f70edcb2c608db025b15583376dcebbdf147590c
Rodrigo Alfonso <rdgdesign@gmail.com>
2022-06-16 10:11:47 -0300

    main

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import '../components/Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <navbar className="nav-header">
+
+        <p>HOME</p>
+        <p>ACERCA DE </p>
+        
+      </navbar>
+    </div>
+  );
+}
+
+export default Header;
